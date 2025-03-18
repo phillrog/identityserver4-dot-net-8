ddcba89d12d6dc6477e722763c196f87492a633a
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-03-18 12:57:19 -0300

    config

--- build-clients.sh
+++ build-clients.sh
@@ -9,7 +9,7 @@ do
     dir=$(cut -d'/' -f2 <<<"$p")
     proj=$(cut -d'/' -f3 <<<"$p")
     if [[ $dir == *"APIs"* ]]; then 
-        dir=$(cut -d'/' -f3 <<<"$p"); 
+        dir=APIs/$(cut -d'/' -f3 <<<"$p"); 
         proj=$(cut -d'/' -f4 <<<"$p")
     fi
     pushd ./$dir
