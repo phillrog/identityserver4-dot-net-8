9d47ccf362baefe1df38dde14b082d5dd9546583
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-03-18 12:18:33 -0300

    config

--- build-clients.sh
+++ build-clients.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 rm -rf nuget
 mkdir nuget
 
 dotnet restore
+
+pushd ./src/
 ls ./src
 dotnet build ./src/IdentityServer4.Clients.sln  --configuration Release --no-restore;    
-
+popd
