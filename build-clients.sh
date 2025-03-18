e5c304f5727c2f90f0550fb376718b9cbb680743
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-03-18 12:00:05 -0300

    config

---
+++ build-clients.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -rf nuget
+mkdir nuget
+
+cd ./src/clients
+
+for p in $(find . -name *.csproj); 
+do 
+    pushd ./$p
+    dotnet restore
+    dotnet build $p  --configuration Release --no-restore; 
+    popd
+done
