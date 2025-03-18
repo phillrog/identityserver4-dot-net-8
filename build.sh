feda4a0d2b0dc9f598ca1144040f66eefd9afb8b
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-03-18 11:13:22 -0300

    config

--- build.sh
+++ build.sh
@@ -4,29 +4,24 @@ set -euo pipefail
 rm -rf nuget
 mkdir nuget
 
-dotnet tool restore
+dotnet restore
 
 pushd ./src/root/Storage
-dotnet restore
 dotnet build --configuration Debug 
 popd
 
 pushd ./src/root/IdentityServer4
-dotnet restore
 dotnet build --configuration Debug 
 popd
 
 pushd ./src/root/EntityFramework.Storage
-dotnet restore
 dotnet build --configuration Debug 
 popd
 
 pushd ./src/root/EntityFramework
-dotnet restore
 dotnet build --configuration Debug 
 popd
 
 pushd ./src/root/AspNetIdentity
-dotnet restore
 dotnet build --configuration Debug 
 popd
