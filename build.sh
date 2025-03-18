1ac7732b278404698690042196b6bbf18f276f3e
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-03-18 10:34:25 -0300

    config

--- build.sh
+++ build.sh
@@ -7,21 +7,21 @@ mkdir nuget
 dotnet tool restore
 
 pushd ./src/root/Storage
-run: dotnet build --configuration Debug --no-restore
+dotnet build --configuration Debug --no-restore
 popd
 
 pushd ./src/root/IdentityServer4
-run: dotnet build --configuration Debug --no-restore
+dotnet build --configuration Debug --no-restore
 popd
 
 pushd ./src/root/EntityFramework.Storage
-run: dotnet build --configuration Debug --no-restore
+dotnet build --configuration Debug --no-restore
 popd
 
 pushd ./src/root/EntityFramework
-run: dotnet build --configuration Debug --no-restore
+dotnet build --configuration Debug --no-restore
 popd
 
 pushd ./src/root/AspNetIdentity
-run: dotnet build --configuration Debug --no-restore
+dotnet build --configuration Debug --no-restore
 popd
