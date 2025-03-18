90bf34b90b753d4a3f330c3076a30a0cd1e45730
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-03-18 12:22:48 -0300

    config

--- build-clients.sh
+++ build-clients.sh
@@ -6,5 +6,10 @@ mkdir nuget
 dotnet restore
 
 pushd ./src
-dotnet build IdentityServer4.Clients.sln  --configuration Release --no-restore;    
+for p in $(find . -name *.csproj); 
+do 
+
+    dotnet build $p  --configuration Release --no-restore; 
+    
+done
 popd
