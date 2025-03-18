502f91c6d8ca42c397150d5bfc9d892d841bacab
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-03-18 12:34:53 -0300

    config

--- build-clients.sh
+++ build-clients.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -euo pipefail
 rm -rf nuget
 mkdir nuget
 cd .src/clients
@@ -8,6 +8,6 @@ for p in $(find . -name *.csproj);
 do 
     pushd ./$(cut -d'/' -f2 <<<"$p")
     dotnet restore
-    dotnet build $p  --configuration Release --no-restore; 
+    dotnet build $(cut -d'/' -f3 <<<"$p")  --configuration Release --no-restore; 
     popd
 done
